b2bfb1480d0ad9f3ff17a1b9b95a9d3f0bea11a3
salsohrab20 <salmansohrab.ansari@gmail.com>
2025-08-08 22:51:38 +0530

    kafka-notes

--- 01-workspace/02-kafka-101/02-producer.sh
+++ 01-workspace/02-kafka-101/02-producer.sh
@@ -1,5 +1,7 @@
 # to produce messages
 kafka-console-producer.sh --bootstrap-server localhost:9092 --topic hello-world
 
-# linger.ms 
+# linger.ms (1000ms - 1 sec) - for reducing the lag in transfering the message from producer to consumer
 kafka-console-producer.sh --bootstrap-server localhost:9092 --topic hello-world --timeout 100
+
+# batch.size(bytes) - 1 more property in producer side that we can configure- it basically implies to pass events after we have already passed events worth this much batch.size
